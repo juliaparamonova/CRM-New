06fa79f4fa031ab896d485ff1d5867c8bd678570
juliaparamonova <jpkoord@gmail.com>
2024-12-25 23:04:16 +0200

    add modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import StatusLabel, { Status } from '@/app/components/status-label';
-import { headers } from 'next/headers';
+// import StatusLabel, { Status } from '@/app/components/status-label';
+// import { headers } from 'next/headers';
+
+// export default function Home() {
+//   console.log(headers());
+//   return (
+//     <main>
+//       <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+//       <StatusLabel status={Status.Active}>Active</StatusLabel>
+//       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+//       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+//       <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+//     </main>
+//   );
+// }
+
+import AddCompanyButton from '@/app/components/add-company-button';
 
 export default function Home() {
-  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page</h1>
+      <AddCompanyButton />
     </main>
   );
 }
