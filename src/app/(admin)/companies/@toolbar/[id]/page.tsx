6b2819b578a448d07a90359a45126ec18ea6bdab
juliaparamonova <jpkoord@gmail.com>
2025-01-05 23:26:00 +0200

    Intercepting Routes

---
+++ src/app/(admin)/companies/@toolbar/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Header from '@/app/components/header';
+
+export interface PageProps {}
+export default function Page({}: PageProps) {
+  return null;
+}
--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,22 +1,7 @@
-'use client';
-import React, { useEffect } from 'react';
-import Header from '@/app/components/header';
-import { notFound } from 'next/navigation';
-
-export interface PageProps {
-  params: { id: string };
-}
-
 export default function Page({ params }: PageProps) {
-  useEffect(() => {
-    const id = Number.parseInt(params.id);
-    if (Number.isNaN(id)) {
-      notFound();
-    }
-  }, [params.id]);
   return (
-    <>
-      <Header>Company ({params.id})</Header>
-    </>
+    <div className="py-6 px-10">
+      <p>{`Information about company (${params.id})`}</p>
+    </div>
   );
 }
