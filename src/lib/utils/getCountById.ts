4249d7f442a6dcc0eebe25990faea856f37cd8e9
juliaparamonova <jpkoord@gmail.com>
2025-01-13 21:55:52 +0200

    upd

--- src/lib/utils/getCountById.ts
+++ src/lib/utils/getCountById.ts
@@ -12,16 +12,4 @@ const getCountById = <T extends string, I extends Record<T, string>>(
   );
 export default getCountById;
 
-// // const getCountById = <T extends string, I extends Record<T, string>>(
-//   items: I[],
-//   idKey: T,
-// ) =>
-//   items.reduce(
-//     (counts, { [idKey]: id }) => {
-//       if (!counts[id]) counts[id] = 0;
-//       counts[id] += 1;
-//       return counts;
-//     },
-//     {} as Record<string, number>,
-//   );
-// export default getCountById;
+// https://65c21c4ff7e6ea59682aa7e1.mockapi.io/api/v1/
