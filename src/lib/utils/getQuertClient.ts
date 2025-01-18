9157261c2a281ef874618da625ab647067dce177
juliaparamonova <jpkoord@gmail.com>
2025-01-18 22:47:49 +0200

    start new theme

--- src/lib/utils/getQuertClient.ts
+++ src/lib/utils/getQuertClient.ts
@@ -1,5 +1,6 @@
 import { cache } from 'react';
 import { QueryClient } from '@tanstack/react-query';
+import {menu} from 
 const getQueryClient = cache(() => new QueryClient()) as () => QueryClient;
 
 // const getQueryClient = cache(() => new QueryClient()) as () => QueryClient;
