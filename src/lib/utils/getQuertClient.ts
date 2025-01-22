ac6c3432eab09e330f6795c71ee870c4e40e1e68
juliaparamonova <jpkoord@gmail.com>
2025-01-22 23:35:53 +0200

    addReact Query

--- src/lib/utils/getQuertClient.ts
+++ src/lib/utils/getQuertClient.ts
@@ -1,8 +1,4 @@
 import { cache } from 'react';
 import { QueryClient } from '@tanstack/react-query';
-import {menu} from 
 const getQueryClient = cache(() => new QueryClient()) as () => QueryClient;
-
-// const getQueryClient = cache(() => new QueryClient()) as () => QueryClient;
-
 export default getQueryClient;
